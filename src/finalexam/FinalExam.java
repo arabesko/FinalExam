e230e40a2918574786c868838dda60a4dbcee3b6
arabesko <123484088+arabesko@users.noreply.github.com>
2026-05-06 10:57:00 +0100

    Created new Project to start the final exam

--- src/finalexam/FinalExam.java
+++ src/finalexam/FinalExam.java
@@ -14,7 +14,8 @@ public class FinalExam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
 }

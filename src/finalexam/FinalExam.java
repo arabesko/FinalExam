593dfeeb0be3c659c87a7c2fa453876ce5b38273
arabesko <123484088+arabesko@users.noreply.github.com>
2026-05-12 11:20:04 +0100

    Menu of the project done

--- src/finalexam/FinalExam.java
+++ src/finalexam/FinalExam.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package finalexam;
-
 /**
  *
  * @author arabesko
@@ -14,6 +13,58 @@ public class FinalExam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Utilities myUtilities = new Utilities();
+        int inputUser = 0;
+        
+        
+        String[] menu = {"Servicios por Cliente", 
+                         "Detalle del Booking",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "Exit"};
+        
+        String textMenu = "\n\n"
+                      + "***** Select a valid option ******"
+                      + "\nOption 1 - " + menu[0]
+                      + "\nOption 2 - " + menu[1]
+                      + "\nOption 3 - " + menu[2]
+                      + "\nOption 4 - " + menu[3]
+                      + "\nOption 5 - " + menu[4]
+                      + "\nOption 6 - " + menu[5]
+                      + "\nOption 7 - " + menu[6]
+                      + "\n\n";
+        
+        while (inputUser != 7){
+            inputUser = myUtilities.myMenu(textMenu);
+            
+            switch(inputUser){
+                case 1 -> {
+                    System.out.println("\n\n" + "************ " + menu[0] + " ************");
+                }
+                case 2 -> {
+                    System.out.println("\n\n" + "************ " + menu[1] + " ************");
+                }
+                case 3 -> {
+                    System.out.println("\n\n" + "************ " + menu[2] + " ************");
+                }
+                case 4 -> {
+                    System.out.println("\n\n" + "************ " + menu[3] + " ************");
+                }
+                case 5 -> {
+                    System.out.println("\n\n" + "************ " + menu[4] + " ************");
+                }
+                case 6 -> {
+                    System.out.println("\n\n" + "************ " + menu[5] + " ************");
+                }
+                case 7 -> {
+                    System.out.println("\n\n" + "************ " + menu[6] + " ************");
+                    System.out.println("See you soon, bye");
+                    System.out.println("\n\n");
+                }
+            }
+        }
         
         
     }

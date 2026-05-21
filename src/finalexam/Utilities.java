5844321187040fb82d481d349d9f187223d39017
arabesko <123484088+arabesko@users.noreply.github.com>
2026-05-21 21:32:13 +0100

    New method to select months in the filters

--- src/finalexam/Utilities.java
+++ src/finalexam/Utilities.java
@@ -45,7 +45,7 @@ public class Utilities {
             System.out.print("Input de option: ");
             try{
                 userInput = myScan.nextInt();
-                if(userInput >= 1 && userInput <=7){
+                if(userInput >= 1 && userInput <=8){
                    valid = true;
                 } else {
                     //If the user input negative number o different to the list
@@ -88,7 +88,7 @@ public class Utilities {
         return userInput;
     }
     
-    public String stringInput(String prompt){
+    public String ascDescInput(String prompt){
         Scanner myScan = new Scanner(System.in);
         int userInput=0;
         String option="";
@@ -117,6 +117,32 @@ public class Utilities {
             }
         } while (valid == false);
         return option;
+    }
+    
+    public int monthSelected(String prompt){
+        Scanner myScan = new Scanner(System.in);
+        int userInput=0;
+        boolean valid = false;
+        
+        do {
+            System.out.print(prompt);
+            try{
+                userInput = myScan.nextInt();
+                if(userInput >= 0 && userInput < 13){
+                   valid = true;
+                } else {
+                    //If the user input negative number
+                    valid = false;
+                    System.out.println("Please input a valid option of the period. try again");
+                }
+            } catch (Exception e){
+                //If the usea iinput text or decimal numbers
+                valid = false;
+                System.out.println("Please input a valid option, try again");
+                myScan.next();
+            }
+        } while (valid == false);
+        return userInput;
     }
     
     public ResultSet dbResult(Connection db, String sqlPromp){
